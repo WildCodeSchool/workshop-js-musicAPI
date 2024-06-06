b8c2146c62d7de551e05858c889c50446b09d50c
brian boudrioux <brian.boudrioux@wildcodeschool.com>
2024-06-06 11:17:48 +0200

    correction step 1 done

--- src/api/albums/controller.js
+++ src/api/albums/controller.js
@@ -9,24 +9,59 @@ const getAll = async ({ res, next }) => {
   }
 };
 
-const getOne = (req, res) => {
-  res.status(200).send("Get One route is OK");
+const getOne = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const [[album]] = await albumsModel.readOne(id);
+    if (album) res.json(album);
+    else res.sendStatus(404);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getTracksByAlbumId = (req, res) => {
-  res.status(200).send("Get Albums route is OK");
+const getTracksByAlbumId = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const [albums] = await albumsModel.readOneByAlbumId(id);
+    res.json(albums);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const postAlbums = (req, res) => {
-  res.status(200).send("Post route is OK");
+const postAlbums = async (req, res, next) => {
+  try {
+    const albumData = req.body;
+    const [result] = await albumsModel.insertOne(albumData);
+    const [[album]] = await albumsModel.readOne(result.insertId);
+    res.status(201).json(album);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const updateAlbums = (req, res) => {
-  res.status(200).send("Update route is OK");
+const updateAlbums = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const albumData = req.body;
+    const [result] = await albumsModel.editOne(id, albumData);
+    if (result.affectedRows > 0) res.sendStatus(204);
+    else res.sendStatus(404);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteAlbums = (req, res) => {
-  res.status(200).send("Delete route is Ok");
+const deleteAlbums = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const [result] = await albumsModel.removeOne(id);
+    if (result.affectedRows > 0) res.sendStatus(204);
+    else res.sendStatus(404);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
--- src/api/albums/model.js
+++ src/api/albums/model.js
@@ -4,8 +4,40 @@ const readAll = () => {
   return db.query("SELECT * FROM albums");
 };
 
+const readOne = (id) => {
+  return db.query("SELECT * FROM albums WHERE id = ?", [id]);
+};
+
+const readOneByAlbumId = (id) => {
+  return db.query(
+    "SELECT track.* FROM track JOIN albums ON track.id_album = albums.id WHERE albums.id = ?",
+    [id],
+  );
+};
+
+const insertOne = (album) => {
+  const { title, genre, picture, artist } = album;
+  return db.query(
+    "INSERT INTO albums (title, genre, picture, artist) VALUES (?, ?, ?, ?)",
+    [title, genre, picture, artist],
+  );
+};
+
+const editOne = (id, album) => {
+  return db.query("UPDATE albums SET ? WHERE id = ?", [album, id]);
+};
+
+const removeOne = (id) => {
+  return db.query("DELETE FROM albums WHERE id = ?", [id]);
+};
+
 // add more function for each action you want to perform on the albums table
 
 module.exports = {
   readAll,
+  readOne,
+  readOneByAlbumId,
+  insertOne,
+  editOne,
+  removeOne,
 };
